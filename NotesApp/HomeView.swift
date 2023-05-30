995149e1fe25bad79cea5a8226045d7b0751101a
Jonathan Tri <jonathantric@gmail.com>
2023-05-30 14:49:17 +0700

    implement fetch notes

--- NotesApp/HomeView.swift
+++ NotesApp/HomeView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var notes = [Note]()
+    
     var body: some View {
         NavigationView {
-            List(0..<9) { i in
-                Text("qwerty \(i)")
+            List(self.notes) { note in
+                Text(note.note)
                     .padding()
             }
+            .onAppear(perform: {
+                fetchNotes()
+            })
             .navigationTitle("Notes")
             .navigationBarItems(trailing: Button(action: {
                 print("Add Notes")
@@ -22,6 +27,30 @@ struct HomeView: View {
             }))
         }
     }
+    
+    func fetchNotes() {
+        let url = URL(string: "http://localhost:3000/notes")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, res, err in
+            guard let data = data else { return }
+            
+            do {
+                let notes = try JSONDecoder().decode([Note].self, from: data)
+                self.notes = notes
+                print(notes)
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct Note: Identifiable, Codable {
+    var id: String { _id }
+    var _id: String
+    var note: String
 }
 
 struct ContentView_Previews: PreviewProvider {
