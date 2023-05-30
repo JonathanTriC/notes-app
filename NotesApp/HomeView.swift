f242489c28bc06300eb2b2d8de73d0482d56f2b2
Jonathan Tri <jonathantric@gmail.com>
2023-05-30 15:28:05 +0700

    implement delete notes

--- NotesApp/HomeView.swift
+++ NotesApp/HomeView.swift
@@ -10,14 +10,33 @@ import SwiftUI
 struct HomeView: View {
     @State var notes = [Note]()
     @State var showAdd = false
+    @State var showAlert = false
+    @State var deleteItem: Note?
+    
+    var alert: Alert {
+        Alert(title: Text("Delete"),
+              message: Text("Are you sure you want to delete this note?"),
+              primaryButton: .destructive(Text("Delete"),
+                                          action: { deleteNote() }),
+              secondaryButton: .cancel())
+    }
     
     var body: some View {
         NavigationView {
             List(self.notes) { note in
                 Text(note.note)
                     .padding()
+                    .onLongPressGesture {
+                        self.showAlert.toggle()
+                        deleteItem = note
+                    }
             }
-            .sheet(isPresented: $showAdd, content: {
+            .alert(isPresented: $showAlert, content: {
+                alert
+            })
+            .sheet(isPresented: $showAdd, onDismiss: {
+                fetchNotes()
+            }, content: {
                 AddNoteView()
             })
             .onAppear(perform: {
@@ -48,6 +67,31 @@ struct HomeView: View {
         }
         
         task.resume()
+    }
+    
+    func deleteNote() {
+        guard let id = deleteItem?._id else { return }
+        let url = URL(string: "http://localhost:3000/notes/\(id)")!
+        
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "DELETE"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, res, err in
+            guard err == nil else { return }
+            guard let data = data else { return }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+        
+        fetchNotes()
     }
 }
 
